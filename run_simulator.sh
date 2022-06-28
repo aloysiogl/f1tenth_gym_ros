6815a8d7ba68dde4891ca071f9e409f335a9e964
Maria <maria-luiza.costa-vianna@inria.fr>
2022-06-28 16:22:50 +0200

    Add run file compatible with particle filter

--- run_simulator.sh
+++ run_simulator.sh
@@ -1,1 +1,5 @@
-sudo docker run -it -p 5901:5901 -p 6901:6901 -p 1025:22 -v ~/aloysiogl/f1tenth_gym_ros:/home/ros/catkin_ws/src/f1tenth_gym_ros --net=host  simulator
+sudo docker run -it -p 5901:5901 -p 6901:6901 -p 1025:22 \
+  -v ~/aloysiogl/particle_filter:/home/ros/catkin_ws/src/particle_filter \
+  -v ~/aloysiogl/f1tenth_gym_ros:/home/ros/catkin_ws/src/f1tenth_gym_ros \
+  --net=host \
+  simulator
